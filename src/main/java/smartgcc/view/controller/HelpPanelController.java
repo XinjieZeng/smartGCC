3cd63161267ccb4606f7496377b1710294e51be4
asoorkeea <cvashwin@hotmail.com>
2020-04-02 23:42:44 -0400

    help foundation

--- src/main/java/smartgcc/view/controller/HelpPanelController.java
+++ src/main/java/smartgcc/view/controller/HelpPanelController.java
@@ -1,14 +1,31 @@
 package smartgcc.view.controller;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.web.WebEngine;
+import javafx.scene.web.WebView;
 import javafx.stage.Stage;
 import smartgcc.MainApp;
 
-public class HelpPanelController {
+public class HelpPanelController implements Initializable {
 
     private MainApp mainApp;
     private Stage dialogueStage;
     private EditorPanelController controller;
 
+    @FXML
+    private WebView viewweb;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+    	WebEngine webengine = viewweb.getEngine();
+    	
+    	webengine.load(getClass().getResource("/help/index.html").toString());
+    }
+    
     public void setDialogueStage(Stage dialogueStage){
         this.dialogueStage = dialogueStage;
     }
